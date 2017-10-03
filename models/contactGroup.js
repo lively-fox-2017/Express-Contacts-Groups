c5dc43b6534b584b1a8a2ed917fd10eb59090396
kholes <cholis.elbazar@gmail.com>
2017-10-03 23:58:27 +0700

    orm final many-to-many modified

--- models/contactGroup.js
+++ models/contactGroup.js
@@ -12,5 +12,16 @@ class ContactGroup{
 			}
 		})
 	}
+	static add(data){
+		let col = []
+		let val = []
+		for(let i in data){
+			col.push(i)
+			val.push(data[i])
+		}
+		let query = `INSERT INTO ContactGroups (${col.join(',')}) VALUES ('${val.join("','")}')`
+		db.run(query)
+	}
+	
 }
 module.exports = ContactGroup
--- routs/group.js
+++ routs/group.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router()
 const Group = require('../models/group')
-const Contact = require('../models/group')
+const Contact = require('../models/contact')
+const ContactGroup = require('../models/contactGroup')
 router.get ('/', function (req, res) {
 	Group.findAll(rows => {
 		res.render('group',{group:rows})
@@ -25,6 +26,14 @@ router.get('/delete/:id', function(req,res){
 	res.redirect('/groups')
 });
 router.get('/assign/:id', function(req,res){
-	
+	Contact.findAll(contacts => {
+		Group.findById(req.params.id, row_group => {
+			res.render('groups_assign', {contact:contacts,group:row_group})
+		})
+	})
+})
+router.post('/assign', function(req,res){
+	ContactGroup.add(req.body)
+	res.redirect('/groups')
 })
 module.exports = router
